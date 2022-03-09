9f9957d93f0d4f0925e3f975bb81c623e56ba74d
chutuanthong <58087825+chutuanthong@users.noreply.github.com>
2022-03-09 17:54:38 +0700

    connect with Mongoose Alatas
    connect with Mongoose Alatas

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const dotenv = require('dotenv');
+
+const mongoose = require('mongoose');
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-console.log(`process.env THOR`, process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('success');
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App listen on port ${port} ...`);
